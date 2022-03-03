613a4e2a34d93c33dba33659b1f177215d3135f2
Sebastian B <sebastian.babinski1@gmail.com>
2022-03-03 14:16:56 +0100

    Added some media styles (480px breakpoint)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,19 @@ export const Navbar = () => {
       <div className="logo-wrapper">
         <img src="assets/flower.png" alt="flower" />
         <p>Renia Flowers</p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="40"
+          height="40"
+          fill="black"
+          className="bi bi-list"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fillRule="evenodd"
+            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+          />
+        </svg>
       </div>
       <nav className="navbar">
         <a href="/">Start</a>
